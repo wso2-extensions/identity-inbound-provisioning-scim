270d70e691103043cb2bd8446a379798ef34a082
DMHP <hasanthidisanayakka@yahoo.com>
2018-05-11 08:47:47 +0530

    Add error message to the scim response

--- components/org.wso2.carbon.identity.scim.provider/src/main/java/org/wso2/carbon/identity/scim/provider/impl/SCIMUserManager.java
+++ components/org.wso2.carbon.identity.scim.provider/src/main/java/org/wso2/carbon/identity/scim/provider/impl/SCIMUserManager.java
@@ -638,9 +638,14 @@ public class SCIMUserManager implements UserManager {
                 if (log.isDebugEnabled()) {
                     log.debug("User: " + newUser.getUserName() + " updated through SCIM.");
                 }
-            } catch (org.wso2.carbon.user.core.UserStoreException e) {
-                throw new CharonException("Error while updating attributes of user: " + newUser.getUserName(), e);
+        } catch (org.wso2.carbon.user.core.UserStoreException e) {
+            String message = "Error while updating attributes of user: " + newUser.getUserName() + ". ";
+            if (StringUtils.isNotBlank(e.getMessage())) {
+                throw new CharonException(message + e.getMessage(), e);
+            } else {
+                throw new CharonException(message, e);
             }
+        }
 
             return newUser;
     }
